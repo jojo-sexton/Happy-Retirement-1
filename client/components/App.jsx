e3950758550fe315dbdf8f477f7168e65c283107
iamjojo <jojolsexton@gmail.com>
2021-12-07 09:53:47 +1300

    iinnital set up

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,6 +5,7 @@ import Nav from './Nav'
 import PingRoutes from './PingRoutes'
 import Registration from './Registration'
 import Users from './Users'
+import Main from './main/Main'
 import { Route } from 'react-router'
 
 function App () {
@@ -12,10 +13,11 @@ function App () {
 
   return (
     <div className='app'>
-      <Route exact path='/' component={Nav} />
+      <Route path='/' component={Nav} />
       <Route exact path='/' component={Users} />
       <Route exact path='/' component={PingRoutes} />
       <Route path='/register' component={Registration} />
+      <Route path='/main' component={Main} />
     </div>
   )
 }
