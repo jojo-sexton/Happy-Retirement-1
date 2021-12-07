3c08e73cddd824868eb5eede1cc752d1db0b2082
iamjojo <jojolsexton@gmail.com>
2021-12-08 09:11:25 +1300

    close and start a new one

--- client/components/main/Lifestyle.jsx
+++ client/components/main/Lifestyle.jsx
@@ -6,12 +6,24 @@ const Lifestyle = () => {
   return (
     <div className='home container'>
       <h2>Step 1 of 4: Choose your retirement lifestyle</h2>
+      <div>
+      <h4>Retirement Standard for a single person aged around 65</p>
 
+      <label>
+      Modest: $598 per week
+      <input
+      type="radio"
+      value="598"
+      />
+      </label>
+      </div>
+      <div>
       <Link to="/savings">
         <button>
           Next step
         </button>
       </Link>
+      </div>
     </div>
   )
 }
